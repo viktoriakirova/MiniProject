e82e3f695b07bd6dcbf6df182a43ab422a100401
V.Kirova24 <V.Kirova24@LAB108PC11>
2024-04-12 12:42:13 +0300

    hgfd

---
+++ MiniProjectPart1/Form4.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace MiniProjectPart1
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            usernameTextBox = new TextBox();
+            passwordTextBox = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(91, 124);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(91, 175);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(91, 228);
+            label3.Name = "label3";
+            label3.Size = new Size(91, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Password:";
+            // 
+            // usernameTextBox
+            // 
+            usernameTextBox.Location = new Point(192, 118);
+            usernameTextBox.Name = "usernameTextBox";
+            usernameTextBox.Size = new Size(150, 31);
+            usernameTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            passwordTextBox.Location = new Point(192, 172);
+            passwordTextBox.Name = "passwordTextBox";
+            passwordTextBox.PasswordChar = '*';
+            passwordTextBox.Size = new Size(150, 31);
+            passwordTextBox.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(192, 228);
+            textBox3.Name = "textBox3";
+            textBox3.PasswordChar = '*';
+            textBox3.Size = new Size(150, 31);
+            textBox3.TabIndex = 5;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(passwordTextBox);
+            Controls.Add(usernameTextBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form4";
+            Text = "Form4";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox usernameTextBox;
+        private TextBox passwordTextBox;
+        private TextBox textBox3;
+    }
+}
